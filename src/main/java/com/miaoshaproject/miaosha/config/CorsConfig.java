458fb043b8fa47b6743936248ebe520b6b210bdf
yangle <yangle_edward@163.com>
2022-06-15 02:58:24 +0800

    fix: fix config of allowCredentials = "true" conflicts config of allowOrigin("*") issue on crossOrigin

---
+++ src/main/java/com/miaoshaproject/miaosha/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.miaoshaproject.miaosha.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author yangLe
+ * @Description TODO
+ * @Date 2022/6/15 0:13
+ * @Version 1.0
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
